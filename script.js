efadcef43db1c07948f1716845304a9a7470f5b1
Rishav Dutta <92175535+Pullu05@users.noreply.github.com>
2022-03-10 17:05:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function sendMoney() {
+   var SName = document.getElementById("SenderName").value;
+   var Amount = parseInt(document.getElementById("enterAmount").value);
+   var RName = document.getElementById("ReceiverName").value;
+
+   var ispresentSender = document.getElementById(SName);
+   var ispresentReceiver = document.getElementById(RName);
+   
+   if (!ispresentSender) {
+      alert("Customar is not in the Customar Data-Base");
+   }
+   else if(!ispresentReceiver){
+      alert("Customar is not in the Customar Data-Base");
+   }
+   else {
+      var SenderTotalAmount = document.getElementById(SName).innerHTML;
+      console.log(SenderTotalAmount);
+      if (Amount > SenderTotalAmount) {
+         alert("Insufficient Balance.")
+      }
+      else {
+
+         var finalAmountofReceiver = parseInt(document.getElementById(RName).innerHTML) + Amount;
+         var finalAmountofSender = parseInt(document.getElementById(SName).innerHTML) - Amount;
+
+
+         document.getElementById(SName).innerHTML = finalAmountofSender;
+         document.getElementById(RName).innerHTML = finalAmountofReceiver;
+
+         alert(`Successful Transaction !!  
+       $${Amount} is sent to ${RName}@email.com.`)
+
+         // transaction history 
+         var createPTag = document.createElement("li");
+         var textNode = document.createTextNode(`Rs ${Amount} is sent from the sender with Email-id ${SName}@email.com
+      to recepient with Email-id ${RName}@email.com on ${Date()}.`);
+         createPTag.appendChild(textNode);
+         var element = document.getElementById("transaction-history-body");
+         element.insertBefore(createPTag, element.firstChild);
+      }
+   }
+}
+
